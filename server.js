b83904abb85de8ec2dd8c5c2aa88ff7009b63933
R4z1ell <creawshaka@gmail.com>
2018-03-09 00:44:10 +0100

    Added project URL

--- server.js
+++ server.js
@@ -94,6 +94,12 @@ app.get("/about", (req, res) => {
   });
 });
 
+app.get("/project", (req, res) => {
+  res.render("project.hbs", {
+    pageTitle: "About Page"
+  });
+});
+
 app.get("/bad", (req, res) => {
   res.send({
     errorMessage: "Unable to fulfill this request"
