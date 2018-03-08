9dfa801af0582f3f28e8a97308d9f40cfd69221b
R4z1ell <creawshaka@gmail.com>
2018-03-09 00:51:26 +0100

    Add project page

--- server.js
+++ server.js
@@ -94,9 +94,9 @@ app.get("/about", (req, res) => {
   });
 });
 
-app.get("/project", (req, res) => {
-  res.render("project.hbs", {
-    pageTitle: "About Page"
+app.get("/projects", (req, res) => {
+  res.render("projects.hbs", {
+    pageTitle: "Projects"
   });
 });
 
